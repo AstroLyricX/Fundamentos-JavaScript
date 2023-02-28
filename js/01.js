97305314ea5e444f765deb16a56781b3baf13d54
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-02-27 23:25:35 -0500

    Add: 01.js - Variables

---
+++ js/01.js
@@ -0,0 +1,22 @@
+
+// ! Variables - let, const
+
+let cliente = 'John';
+
+const precioDescuento = 10; //camelCase
+
+
+// ? Re asignar
+
+cliente = 20;
+cliente = true;
+
+// ? Pueden iniciar sin un valor
+
+let precio;
+precio = 200;
+
+
+console.log(precio);
+console.log(cliente);
+console.log(precioDescuento);
