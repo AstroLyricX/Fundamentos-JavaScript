0df5db9171bc6ed433504e2242854ce20ed07072
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-03-01 15:13:54 -0500

    Update: 12.js - Funciones - Function Declaration - return value

--- js/12.js
+++ js/12.js
@@ -14,3 +14,14 @@ sumar2();
 sumar2(10,20);
 sumar2(100, 400);
 sumar2(15);
+
+function sumar3(numero = 0, numero2 = 0 ) {
+    return {
+        resultado: numero + numero2,
+        mensaje: 'Hola Mundo',
+    }
+}
+
+const { resultado, mensaje } = sumar3(20, 30);
+console.log(resultado);
+console.log(mensaje);
