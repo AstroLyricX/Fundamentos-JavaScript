c076787e5016e9ae1b21d6ce3bbf551e5e90e4dc
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-03-01 15:25:32 -0500

    Add: 13.js - Funciones - Function Expression

---
+++ js/13.js
@@ -0,0 +1,11 @@
+// ! Funciones - Function Expression
+
+//* sumar() - No ❌
+
+const sumar = function( numero = 0, numero2 = 0 ) {
+    return numero + numero2;
+}
+
+const resultado = sumar(30);
+
+console.log(resultado);
