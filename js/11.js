042d2d894e590c6e66f52368ebf55c1744ed5a86
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-03-01 14:31:31 -0500

    Add: 11.js - Iteradores en JS

---
+++ js/11.js
@@ -0,0 +1,12 @@
+// ! Iteradores en JS
+
+const tecnologia = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+
+// ? ForEach - Acceder a cada elemento del Array
+const arrayForeach = tecnologia.forEach((tech) => tech);
+
+// ? Map - Crea un nuevo Array
+const arrayMap = tecnologia.map((tech) => tech);
+
+console.log(arrayForeach);
+console.log(arrayMap);
