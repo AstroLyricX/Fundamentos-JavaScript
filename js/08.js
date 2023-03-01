6634657ac07c9c499c89f256664a23c8073c68e9
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-03-01 12:21:51 -0500

    Add: 08.js - Arrays o Arreglos

---
+++ js/08.js
@@ -0,0 +1,8 @@
+// ! Arrays o Arreglos
+
+const tecnologias = [30, 20, true, 'React', 'JavaScript'];
+
+console.log(tecnologias[9]);
+console.table(tecnologias);
+console.log(tecnologias.length);
+console.log(tecnologias.toString());
