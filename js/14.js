24a8886fa44e7970b61c014c1e8f23baed7b3d89
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-03-02 10:41:28 -0500

    Add: 14.js - Funciones - Arrow Functions

---
+++ js/14.js
@@ -0,0 +1,11 @@
+// ! Funciones - Arrow Functions
+
+const sumar = ( numero = 0, numero2 = 0 ) => numero + 20;
+
+const sumarArrow = () => 2 + 2;
+
+const resultado = sumar(30);
+
+console.log(resultado);
+
+console.log(sumarArrow());
